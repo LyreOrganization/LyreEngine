64c0cd3895c0099ef85cf8602f3a0b8f7597155a
Umpire <umpire333@gmail.com>
2020-04-30 12:46:26 +0300

    Now we using premake for project configuration.

---
+++ premake5.lua
@@ -0,0 +1,91 @@
+workspace "LyreEngine"
+    architecture "x86_64"
+    configurations { "Debug", "Release" }
+
+    startproject "Launch"
+
+outputdir = "%{cfg.architecture}_%{cfg.system}_%{cfg.buildcfg}"
+
+project "LyreEngine"
+    location "code/%{prj.name}"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+	pchheader "LyrePch.h"
+	pchsource "code/%{prj.name}/src/LyrePch.cpp"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/objs/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.location}/src/**.h",
+        "%{prj.location}/src/**.cpp"
+    }
+
+	includedirs
+	{
+		"%{prj.location}/src"
+	}
+
+    filter "system:windows"
+        defines { "LYRE_PLATFORM_WINDOWS" }
+        systemversion "latest"
+        
+        files {
+            "%{prj.location}/platform/win64/**"
+        }
+
+    filter "configurations:Debug"
+        defines { "LYRE_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "LYRE_RELEASE" }
+        optimize "On"
+
+project "Launch"
+    location "code/%{prj.name}"
+    kind "WindowedApp"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin/objs/" .. outputdir .. "/%{prj.name}")
+    
+    files {
+        "%{prj.location}/src/**.h",
+        "%{prj.location}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "code/LyreEngine/src"
+    }
+
+	links
+	{
+		"LyreEngine"
+	}
+
+    filter "system:windows"
+        defines { "LYRE_PLATFORM_WINDOWS" }
+		systemversion "latest"
+        files {
+            "%{prj.location}/entry/win64/**.h",
+            "%{prj.location}/entry/win64/**.cpp"
+        }
+		includedirs
+		{
+			"code/LyreEngine/platform/win64"
+		}
+
+    filter "configurations:Debug"
+        defines { "LYRE_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "LYRE_RELEASE" }
+        optimize "On"
